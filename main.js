3a7557ecfdd0bd0cb46ab2c335f90d4b8353aa32
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-10-25 16:32:42 -0300

    TUDO

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-// criei a função tocaSomAplausos
-function tocaSomAplausos(idElementoAudio){
-    document.querySelector(idElementoAudio).play();}
+//criei a função tocaSomAplausos e usamos o .play para tocar o som do id
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play(); 
+ }
+ //criei constante listaDeTeclas e busquei a classe coletiva tecla
+  const listaDeTeclas = document.querySelectorAll(".tecla");
+ //criei a variavel contador
+ let contador = 0;
+ //criei o laço de repetição while chamando cada botão da lista de teclas pelo contador
+ while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_" + efeito;
+    listaDeTeclas[contador].onclick = function(){
+       tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    //console.log(contador);
+ }        
 
-// criei a constante listaDeTeclas e busquei a classe coletiva tecla
-const listaDeTeclas= document.querySelectorAll(".tecla");
 
-//criei variável contador
-let contador=0;
-//criei o laço de repetição while, com a condicional somar um para o contador
-while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick=tocaSom;
-    contador = contador + 1; 
-    console.log (contador);
-}
+        
