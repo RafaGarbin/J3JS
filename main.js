9d6d0dcd642f0b630bf2e97ebb5e2afc1f537319
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-10-25 15:45:17 -0300

    oi

--- main.js
+++ main.js
@@ -1,48 +1,15 @@
+// criei a função tocaSomAplausos
 function tocaSomAplausos(){
     document.querySelector("#som_tecla_aplausos").play();}
 
-    document.querySelector(".tecla_aplausos").onclick=tocaSomAplausos;
+// criei a constante listaDeTeclas e busquei a classe coletiva tecla
+const listaDeTeclas= document.querySelectorAll(".tecla");
 
-function tocaSomVaia(){
-    document.querySelector("#som_tecla_vaia").play();}
-    
-    document.querySelector(".tecla_vaia").onclick=tocaSomVaia;
-
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();}
-        
-    document.querySelector(".tecla_risada").onclick=tocaSomRisada;
-
-
-
-function tocaSomErrado(){
-    document.querySelector("#som_tecla_errado").play();}
-            
-    document.querySelector(".tecla_errado").onclick=tocaSomErrado;
-
-function tocaSomGrilo(){
-    document.querySelector("#som_tecla_grilo").play();}
-                
-    document.querySelector(".tecla_grilo").onclick=tocaSomGrilo;
-
-function tocaSomTambores(){
-    document.querySelector("#som_tecla_tambores").play();}
-                    
-    document.querySelector(".tecla_tambores").onclick=tocaSomTambores;
-
-
-
-function tocaSomTrombeta(){
-    document.querySelector("#som_tecla_trombeta").play();}
-                    
-    document.querySelector(".tecla_trombeta").onclick=tocaSomTrombeta;
-            
-function tocaSomDinheiro(){
-    document.querySelector("#som_tecla_dinheiro").play();}
-                        
-    document.querySelector(".tecla_dinheiro").onclick=tocaSomDinheiro;
-        
-function tocaSomVitoria(){
-    document.querySelector("#som_tecla_vitoria").play();}
-                            
-    document.querySelector(".tecla_vitoria").onclick=tocaSomVitoria;
+//criei variável contador
+let contador=0;
+//criei o laço de repetição while, com a condicional somar um para o contador
+while (contador<9){
+    listaDeTeclas[0].onclick=tocaSomAplausos;
+    contador = contador +1; 
+    console.log (contador);
+}
