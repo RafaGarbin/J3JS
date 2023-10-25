29c273217996fb5f56e146e6b5c3d03749dd0d6b
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-10-25 15:55:03 -0300

    oi

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listaDeTeclas= document.querySelectorAll(".tecla");
 //criei variável contador
 let contador=0;
 //criei o laço de repetição while, com a condicional somar um para o contador
-while (contador< listaDeTeclas.length){
+while (contador < listaDeTeclas.length){
     listaDeTeclas[0].onclick=tocaSomAplausos;
     contador = contador + 1; 
     console.log (contador);
